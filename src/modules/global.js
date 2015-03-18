ef9555e62696a5f8e0c57806682c28c38920b7ac
Martin Gontovnikas <martin@gon.to>
2015-03-18 16:25:11 -0300

    New version

--- src/modules/global.js
+++ src/modules/global.js
@@ -6,17 +6,19 @@ angular.module('ux.global', [])
     this.registerPage = function(name, home) {
       this.pages.push(name);
       if (home || $location.search().page == name) {
-        this.changePage(name);
+        this.changePage(name, !$location.search().page);
       }
     };
 
-    this.changePage = function(name) {
+    this.changePage = function(name, changeUrl) {
       if (this.pages.indexOf(name) < 0) {
         throw new Error("The page you want to go to doesn't exist");
       }
-      var params = $location.search();
-      params.page = name;
-      $location.search(params);
+      if (changeUrl) {
+        var params = $location.search();
+        params.page = name;
+        $location.search(params);
+      }
       this.currentPage = name;
     };
   })
